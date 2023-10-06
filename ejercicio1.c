ff41847df0571d608240f29c93a658ce84df4903
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-06 00:24:12 -0500

    prueba

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    
+    int numero;
+    numero=0;
+    printf("Ingrese un numero entero:");
+    scanf("%d",&numero);
+    printf("El numero es primo");
     return 0;
 }
