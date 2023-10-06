ae3314c35e04d546f2e3767c3cdcd269e3507144
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2023-10-06 01:09:23 -0500

    programa acabado

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 int main()
 {
-    int numero;
+    int numero,div,veces;
     numero=0;
+    div=1;
+    veces=0;
     printf("Ingrese un numero entero:");
     scanf("%d",&numero);
-    printf("El numero es primo");
+    while (div<=numero)
+    {
+        if (numero % div==0){
+            veces=veces+1;
+            
+        }
+        div=div+1;
+    }
+    if (veces==2)
+    {
+        printf("El numero es primo");
+    }else{
+        printf("El numero no es primo");
+    }
     return 0;
 }
